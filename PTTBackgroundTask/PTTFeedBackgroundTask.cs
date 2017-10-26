e152014919e870ed379c0081f9c81630fa757db2
lightyen <lightyen0123@gmail.com>
2017-10-26 16:32:02 +0800

    LiPTT

---
+++ PTTBackgroundTask/PTTFeedBackgroundTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace PTTBackgroundTask
+{
+    public sealed class PTTFeedBackgroundTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+
+        }
+    }
+}
